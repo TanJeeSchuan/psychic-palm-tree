fc31ccd6e5cdce92445eea8df17d478c456dd157
TanJeeSchuan <tanjeeschuan@gmail.com>
2023-05-22 20:45:18 +0800

    Added combat function header and c files

---
+++ main/combat_functions.h
@@ -0,0 +1,6 @@
+#pragma once
+int dice_roll(int, int);
+int unit_pips(int, int, int, int, int, int);
+float casualty_multiplier(int, float, float, float, float, int);
+float morale_casualties(int, float, float, float);
+float strength_casualties(int, float, float, float);
